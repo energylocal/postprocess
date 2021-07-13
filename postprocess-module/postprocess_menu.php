e4335c101d3ec8ff8fe2575e65395bfe39fdbecd
TrystanLea <trystan.lea@gmail.com>
2021-07-13 20:53:05 +0000

    hide menu

--- postprocess-module/postprocess_menu.php
+++ postprocess-module/postprocess_menu.php
@@ -1,2 +1,2 @@
 <?php
-if ($session["write"]) $menu["setup"]["l2"]['postprocess'] = array("name"=>"Post Process","href"=>"postprocess", "order"=>6, "icon"=>"spinner11");
+if ($session["admin"]) $menu["setup"]["l2"]['postprocess'] = array("name"=>"Post Process","href"=>"postprocess", "order"=>6, "icon"=>"spinner11");
